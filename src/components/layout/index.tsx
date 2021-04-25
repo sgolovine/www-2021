5df8a4492daf8a57f31a97f0e645ce1e03398c56
Sunny Golovine <sunny@glvn.co>
2021-04-25 12:09:41 -0400

    Responsive mobile menu

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,22 +1,39 @@
-import React, { ReactNode } from "react"
+import React, { ReactNode, useEffect, useState } from "react"
+import { CloseIcon } from "~/icons/Close"
+import { Menu } from "~/icons/Menu"
 import Header from "./Header"
 import Sidebar from "./Sidebar"
+import useMedia from "use-media"
 
 interface Props {
   children: ReactNode
 }
 
 const Layout: React.FC<Props> = ({ children }) => {
+  const showMobileMenu = useMedia({ maxWidth: "767px" })
+  const [mobMenuOpen, setMobMenuOpen] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (!showMobileMenu) {
+      setMobMenuOpen(false)
+    }
+  }, [showMobileMenu])
+
   return (
     // Outer Container
     <div className="max-w-3xl mx-auto py-6">
       {/* Header */}
-      <div className="text-right py-16">
+      <div className="text-left px-4 md:px-0 md:text-right py-8 md:py-16 flex flex-row justify-between items-center">
         <Header />
-        <div className="block md:hidden">
-          <Sidebar />
-        </div>
+        <button
+          onClick={() => setMobMenuOpen(!mobMenuOpen)}
+          className="block md:hidden"
+        >
+          {mobMenuOpen ? <CloseIcon /> : <Menu />}
+        </button>
+        {/* <Menu /> */}
       </div>
+      {mobMenuOpen && showMobileMenu && <Sidebar />}
 
       {/* Primary Content */}
       <div className="flex flex-row">
--- src/icons/ExternalLink.tsx
+++ src/icons/ExternalLink.tsx
@@ -1,7 +1,6 @@
-import React from "react";
-import { IconProps } from "~/model/IconProps";
+import React from "react"
 
-export const ExternalLinkIcon: React.FC<IconProps> = ({
+export const ExternalLinkIcon: React.FC<{ className?: string }> = ({
   className = "h-6 w-6",
 }) => (
   <svg
@@ -18,7 +17,7 @@ export const ExternalLinkIcon: React.FC<IconProps> = ({
       d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
     />
   </svg>
-);
+)
 
 export const ExternalLinkIcon2 = () => {
   return (
@@ -36,5 +35,5 @@ export const ExternalLinkIcon2 = () => {
         d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
       />
     </svg>
-  );
-};
+  )
+}
