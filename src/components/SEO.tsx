33ec25f92945361ef318bd5cf936cff5c614ced8
Sunny Golovine <sunny@glvn.co>
2021-04-27 16:21:51 -0400

    Add viewport and more open graph tags

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -14,16 +14,19 @@ const SEO = () => {
       <html lang="en" />
       <title>{title}</title>
       <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="description" content={description} />
       <meta name="robots" content={robots} />
       <meta name="keywords" content={keywords} />
 
       {/* Open Graph (Facebook) */}
       <meta name="og:title" content={title} />
-      <meta name="og:type" content="website" />
-      <meta name="og:url" content="https://sunnygolovine.com" />
       <meta name="og:description" content={description} />
       <meta name="og:image" content={image} />
+      <meta name="og:image:alt" content="Sunny Golovine" />
+      <meta property="og:locale" content="en_US" />
+      <meta name="og:type" content="website" />
+      <meta name="og:url" content="https://sunnygolovine.com" />
 
       {/* Twitter */}
       <meta name="twitter:card" content="website" />
