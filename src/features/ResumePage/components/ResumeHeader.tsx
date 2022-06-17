77aedca4fec55c6ab544678287da7479baf01f6e
Sunny Golovine <sunny@glvn.co>
2022-06-17 11:35:16 -0600

    Move around components to colocate with features

--- src/features/ResumePage/components/ResumeHeader.tsx
+++ src/features/ResumePage/components/ResumeHeader.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Button } from "../common/Button"
+import { Button } from "~/components/common/Button"
 import { Header } from "~/components/common/Typography"
 
 const ResumePageHeader = () => {
