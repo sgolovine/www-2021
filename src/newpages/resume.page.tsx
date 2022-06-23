5cf277d5010533c5bee4a89a878bde345262d19d
Sunny Golovine <sunny@glvn.co>
2022-06-22 18:35:42 -0600

    Setup scripts

--- src/newpages/resume.page.tsx
+++ src/newpages/resume.page.tsx
@@ -1,3 +1,5 @@
+import React from "react"
+
 const PAGE_NAME = "Resume"
 
 const Page = () => <p>{PAGE_NAME}</p>
--- src/newpages/work.page.tsx
+++ src/newpages/work.page.tsx
@@ -1,3 +1,5 @@
+import React from "react"
+
 const PAGE_NAME = "Work"
 
 const Page = () => <p>{PAGE_NAME}</p>
