335bf27434ccd3e822a9a8d2d6142d5e601c7c02
Sunny Golovine <sunny@glvn.co>
2022-07-08 17:19:13 -0600

    Remove dead code

--- src/components/common/SEO.tsx
+++ src/components/common/SEO.tsx
@@ -1,13 +1,6 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 
-interface PostSEOProps {
-  title: string
-  description: string
-  path: string
-  date?: string
-}
-
 const title = "Sunny Golovine"
 const description = "Personal website of Sunny Golovine."
 const robots = "index, follow"
@@ -42,31 +35,3 @@ export const SiteSEO = () => (
     <meta name="twitter:image" content={image} />
   </>
 )
-
-export const PostSEO: React.FC<PostSEOProps> = ({
-  title,
-  description,
-  path,
-  date,
-}) => {
-  const formattedDate = date ? new Date(date).toISOString() : null
-  const canonicalURL = `https://sunnygolovine.com${path}`
-  return (
-    <Helmet>
-      <title>{title}</title>
-      <meta name="og:title" content={title} />
-      <meta name="twitter:title" content={title} />
-
-      <meta name="description" content={description} />
-      <meta name="og:description" content={description} />
-      <meta name="twitter:description" content={description} />
-
-      <meta name="og:author" content="Sunny Golovine" />
-      {formattedDate && (
-        <meta name="og:published_time" content={formattedDate} />
-      )}
-
-      <link rel="canonical" href={canonicalURL} />
-    </Helmet>
-  )
-}
