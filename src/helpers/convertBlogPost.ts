7d9ac7b16f27739282d5bedb6ccc729d524d0f24
Sunny Golovine <sunny@glvn.co>
2022-07-08 11:29:43 -0600

    Remove unused code and packages

--- src/helpers/convertBlogPost.ts
+++ src/helpers/convertBlogPost.ts
@@ -1,17 +1,14 @@
 import { BlogPost, RawBlogPost } from "~/model/BlogPost"
 
-/** Converts a "raw" blog post into a regular one */
-export function convertBlogPost(post: RawBlogPost): BlogPost {
-  if (post.rawDate) {
-    return {
-      ...post,
-      date: new Date(post.rawDate),
-    }
-  }
-  // no-op for snippets
-  return post
-}
-
 export function convertBlogPosts(posts: RawBlogPost[]): BlogPost[] {
-  return posts.map(post => convertBlogPost(post))
+  return posts.map(post => {
+    if (post.rawDate) {
+      return {
+        ...post,
+        date: new Date(post.rawDate),
+      }
+    }
+    // no-op for snippets
+    return post
+  })
 }
